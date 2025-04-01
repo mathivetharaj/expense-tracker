7014a9d132074c3e4316e2cf4e034e4e65a95458
Mathi Vetharaj <mathi.vetharaj@gmail.com>
2025-04-01 23:46:52 +0400

    initial version of restaurant expense tracker

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app=express();
+const port= 3000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
